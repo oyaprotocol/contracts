0a3e25c8fa61194bf91ccdee24d5215367d6f2fa
John Shutt <pemulis@users.noreply.github.com>
2024-03-18 14:45:06 -0700

    fix safe test
    Signed-off-by: John Shutt <pemulis@users.noreply.github.com>

--- test/SafeTest.t.sol
+++ test/SafeTest.t.sol
@@ -2,13 +2,14 @@ pragma solidity ^0.8.20;
 
 import "safe-tools/SafeTestTools.sol";
 import "forge-std/Test.sol";
+import "forge-std/console.sol";
 
 contract SafeTest is Test, SafeTestTools {
     using SafeTestLib for SafeInstance;
 
     function setUp() public {
-        vm.createSelectFork(vm.envString("${MAINNET_RPC_URL}"));
-        address alice = vm.addr(1);
+        vm.createSelectFork(vm.envString("MAINNET_RPC_URL"));
+        address alice = vm.addr(1337);
 
         address frax_safe = 0xB1748C79709f4Ba2Dd82834B8c82D4a505003f27;
         SafeInstance memory safeInstance = _attachToSafe(frax_safe);
@@ -18,7 +19,10 @@ contract SafeTest is Test, SafeTestTools {
             value: 0.5 ether,
             data: ""
         }); // send .5 eth to alice
+    }
 
+    function testSafe() public {
+        address alice = vm.addr(1337);
         assertEq(alice.balance, 0.5 ether); // passes ✅
     }
 }
