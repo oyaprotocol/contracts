12c6edcfb32e251b82d3f3f2e7bbc93da252c2ff
John Shutt <pemulis@users.noreply.github.com>
2024-03-20 13:50:49 -0700

    stub module deployment test
    Signed-off-by: John Shutt <pemulis@users.noreply.github.com>

---
+++ test/SafeModuleDeployTest.t.sol
@@ -0,0 +1,22 @@
+import "safe-tools/SafeTestTools.sol";
+import "forge-std/Test.sol";
+
+contract SafeModuleDeployTest is Test, SafeTestTools {
+    using SafeTestLib for SafeInstance;
+
+    function setUp() public {
+        SafeInstance memory safeInstance = _setupSafe();
+        address alice = address(0xA11c3);
+
+        safeInstance.execTransaction({
+            to: alice,
+            value: 0.5 ether,
+            data: ""
+        }); // send .5 eth to alice
+    }
+
+    function testSafe() public {
+        address alice = address(0xA11c3);
+        assertEq(alice.balance, 0.5 ether); // passes ✅
+    }
+}
