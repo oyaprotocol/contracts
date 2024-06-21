fd1ec9b63d36db4f2b8bece1d3dfdb5eb54f2741
John Shutt <pemulis@users.noreply.github.com>
2024-06-21 13:59:03 -0700

    mark module contract as old
    Signed-off-by: John Shutt <pemulis@users.noreply.github.com>

--- src/implementation/old/OyaModule.sol
+++ src/implementation/old/OyaModule.sol
@@ -6,7 +6,7 @@ import "@gnosis.pm/zodiac/contracts/core/Module.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
-import "./OptimisticProposer.sol";
+import "../OptimisticProposer.sol";
 
 /**
  * @title Oya Module
