30b2e29cec70df22ec5bb8bdc535f95106a76f50
John Shutt <pemulis@users.noreply.github.com>
2024-09-29 15:52:23 -0700

    clean up bookkeeper contract
    Signed-off-by: John Shutt <pemulis@users.noreply.github.com>

--- src/implementation/Bookkeeper.sol
+++ src/implementation/Bookkeeper.sol
@@ -4,55 +4,33 @@ import "@gnosis.pm/safe-contracts/contracts/base/Executor.sol";
 
 import "./OptimisticProposer.sol";
 
-/// @title Bookkeeper
-/// @dev Holds assets on behalf of account holders in the Oya network.
 contract Bookkeeper is OptimisticProposer, Executor {
-
   using SafeERC20 for IERC20;
-
-  event BookkeeperDeployed(string rules);
-
-  event BookkeeperUpdated(address indexed contractAddress, uint256 indexed chainId, bool isApproved);
-
-  event SetAccountRules(address indexed account, string accountRules);
-
-  event SetController(address indexed account, address indexed controller);
-
-  event SetGuardian(address indexed account, address indexed guardian);
-
-  event RecoverAccount(address indexed account, address indexed newAccount);
-
-  event ChangeAccountMode(address indexed account, AccountMode mode, uint256 timestamp);
 
   enum AccountMode { Automatic, Manual, Frozen }
 
-  /// @notice Mapping of Bookkeeper contract address to chain IDs, and whether they are authorized.
-  mapping(address => mapping(uint256 => bool)) public bookkeepers;
+  event BookkeeperDeployed(string rules);
+  event BookkeeperUpdated(address indexed contractAddress, uint256 indexed chainId, bool isApproved);
+  event ChangeAccountMode(address indexed account, AccountMode mode, uint256 timestamp);
+  event SetAccountRules(address indexed account, string accountRules);
+  event SetController(address indexed account, address indexed controller);
+  event SetGuardian(address indexed account, address indexed guardian);
 
+  mapping(address => mapping(uint256 => bool)) public bookkeepers;
   mapping(address => string) public accountRules;
   mapping(address => AccountMode) public accountModes;
-
-  // Time at which manual mode is active. 15 minute delay to switch from automatic to manual.
-  // If set to 0, the account is not in manual mode.
-  mapping(address => uint256) public manualModeLiveTime;
-
   mapping(address => mapping(address => bool)) public isController;
   mapping(address => mapping(address => bool)) public isGuardian;
+
+  // Timestamp at which manual mode is active. 15 minute delay to switch from automatic to manual.
+  // If set to 0, the account is not in manual mode.
+  mapping(address => uint256) public manualModeLiveTime;
 
   modifier notFrozen(address _account) {
     require(getCurrentMode(_account) != AccountMode.Frozen, "Account is frozen");
     _;
   }
 
-  /**
-   * @notice Construct Oya Bookkeeper contract.
-   * @param _finder UMA Finder contract address.
-   * @param _collateral Address of the ERC20 collateral used for bonds.
-   * @param _bondAmount Amount of collateral currency to make assertions for proposed transactions
-   * @param _rules Reference to the Oya global rules.
-   * @param _identifier The approved identifier to be used with the contract, compatible with Optimistic Oracle V3.
-   * @param _liveness The period, in seconds, in which a proposal can be disputed.
-   */
   constructor(
     address _finder,
     address _collateral,
@@ -67,10 +45,6 @@ contract Bookkeeper is OptimisticProposer, Executor {
     setUp(initializeParams);
   }
 
-  /**
-   * @notice Sets up the Oya Bookkeeper contract.
-   * @param initializeParams ABI encoded parameters to initialize the contract with.
-   */
   function setUp(bytes memory initializeParams) public initializer {
     _startReentrantGuardDisabled();
     __Ownable_init();
@@ -85,30 +59,6 @@ contract Bookkeeper is OptimisticProposer, Executor {
     emit BookkeeperDeployed(_rules);
   }
 
-  function getCurrentMode(address _account) public view returns (AccountMode) {
-    AccountMode mode = accountModes[_account];
-    if (mode == AccountMode.Manual && block.timestamp < manualModeLiveTime[_account]) {
-      // Manual mode is scheduled but not yet active; treat as Automatic
-      return AccountMode.Automatic;
-    }
-    return mode;
-  }
-
-  /// @notice Updates the address of a Bookkeeper contract for a specific chain.
-  /// @dev Only callable by the contract owner. Bookkeepers are added by protocol governance.
-  /// @dev There may be multiple Bookkeepers on one chain temporarily during a migration.
-  /// @param _contractAddress The address of the Bookkeeper contract.
-  /// @param _chainId The chain to update.
-  /// @param _isApproved Set to true to add the Bookkeeper contract, false to remove.
-  function updateBookkeeper(address _contractAddress, uint256 _chainId, bool _isApproved) external onlyOwner {
-    bookkeepers[_contractAddress][_chainId] = _isApproved;
-    emit BookkeeperUpdated(_contractAddress, _chainId, _isApproved);
-  }
-
-  /**
-   * @notice Executes an approved proposal.
-   * @param transactions the transactions being executed. These must exactly match those that were proposed.
-   */
   function executeProposal(Transaction[] memory transactions) external nonReentrant {
     // Recreate the proposal hash from the inputs and check that it matches the stored proposal hash.
     bytes32 proposalHash = keccak256(abi.encode(transactions));
@@ -142,6 +92,15 @@ contract Bookkeeper is OptimisticProposer, Executor {
     emit ProposalExecuted(proposalHash, assertionId);
   }
 
+  function getCurrentMode(address _account) public view returns (AccountMode) {
+    AccountMode mode = accountModes[_account];
+    if (mode == AccountMode.Manual && block.timestamp < manualModeLiveTime[_account]) {
+      // Manual mode is scheduled but not yet active; treat as Automatic
+      return AccountMode.Automatic;
+    }
+    return mode;
+  }
+
   function setController(address _account, address _controller) external notFrozen(_account) {
     require(msg.sender == _account || isController[_account][msg.sender], "Not a controller");
     isController[_account][_controller] = true;
@@ -154,10 +113,6 @@ contract Bookkeeper is OptimisticProposer, Executor {
     emit SetGuardian(_account, _guardian);
   }
 
-  /**
-   * @notice Sets the rules that will be used to evaluate future proposals from this account.
-   * @param _rules string that outlines or references the location where the rules can be found.
-   */
   function setAccountRules(address _account, string memory _rules) external notFrozen(_account) {
     require(msg.sender == _account || isController[_account][msg.sender], "Not a controller");
     // Set reference to the rules for the Oya module
@@ -202,10 +157,8 @@ contract Bookkeeper is OptimisticProposer, Executor {
     }
   }
 
-  // Account recovery is done on the virtual chain. A proposed bundle can include account recovery
-  // instructions, sweeping all virtual chain assets from a compromised account address to a new 
-  // address. If the proposed recovery does not follow the global rules and account rules, the 
-  // proposal will be rejected. No need for an additional function here, I think. Any funds deposited
-  // from this address in the future will be assigned to the new address on the virtual chain.
-
+  function updateBookkeeper(address _contractAddress, uint256 _chainId, bool _isApproved) external onlyOwner {
+    bookkeepers[_contractAddress][_chainId] = _isApproved;
+    emit BookkeeperUpdated(_contractAddress, _chainId, _isApproved);
+  }
 }
