634b15a7bd14197f0d38d9a0bc2810c89845c533
John Shutt <pemulis@users.noreply.github.com>
2025-01-30 20:43:37 -0800

    add some more tests
    Signed-off-by: John Shutt <pemulis@users.noreply.github.com>

--- test/BlockTracker.t.sol
+++ test/BlockTracker.t.sol
@@ -2,7 +2,8 @@
 pragma solidity ^0.8.6;
 
 import "forge-std/Test.sol";
-// You might already have these imported; adjust paths as necessary
+import "@gnosis.pm/safe-contracts/contracts/common/Enum.sol";
+
 import "@uma/core/optimistic-oracle-v3/interfaces/EscalationManagerInterface.sol";
 import "../src/implementation/BlockTracker.sol";
 import "../src/mocks/MockAddressWhitelist.sol";
@@ -10,7 +11,6 @@ import "../src/mocks/MockERC20.sol";
 import "../src/mocks/MockFinder.sol";
 import "../src/mocks/MockIdentifierWhitelist.sol";
 import "../src/mocks/MockOptimisticOracleV3.sol";
-import "@gnosis.pm/safe-contracts/contracts/common/Enum.sol";
 
 contract BlockTrackerTest is Test {
     BlockTracker public blockTracker;
@@ -22,15 +22,20 @@ contract BlockTrackerTest is Test {
     MockERC20 public newMockCollateral;
     EscalationManagerInterface public mockEscalationManager;
 
-    address public owner = address(1);
+    address public owner         = address(1);
     address public blockProposer = address(2);
-    address public randomAddress = address(3);
+    address public txProposer    = address(3); // For proposeTransactions
+    address public randomAddress = address(4);
 
     uint256 public bondAmount = 1000;
-    string public rules = "Sample rules";
-    string public newRules = "New rules";
+    string  public rules      = "Sample rules";
+    string  public newRules   = "New rules";
+
     bytes32 public identifier = keccak256("Identifier");
-    uint64 public liveness = 100;
+    bytes32 public newIdentifier = keccak256("AnotherIdentifier");
+    
+    uint64 public liveness    = 100;
+    uint64 public newLiveness = 200;
 
     function setUp() public {
         // Set up the mock contracts
@@ -50,6 +55,7 @@ contract BlockTrackerTest is Test {
         mockAddressWhitelist.addToWhitelist(address(mockCollateral));
         mockAddressWhitelist.addToWhitelist(address(newMockCollateral));
         mockIdentifierWhitelist.addIdentifier(identifier);
+        mockIdentifierWhitelist.addIdentifier(newIdentifier);
 
         // Deploy & initialize the BlockTracker contract from "owner" address
         vm.startPrank(owner);
@@ -63,6 +69,10 @@ contract BlockTrackerTest is Test {
         );
         vm.stopPrank();
     }
+
+    // -----------------------------------------
+    // Tests for native BlockTracker functions
+    // -----------------------------------------
 
     function testProposeBlock() public {
         // Give blockProposer some collateral to pay bond
@@ -78,14 +88,16 @@ contract BlockTrackerTest is Test {
         blockTracker.proposeBlock(blockData);
 
         // The contract stores in: blocks[block.timestamp][msg.sender]
-        // So we can read it back:
         string memory storedData = blockTracker.blocks(block.timestamp, blockProposer);
         assertEq(storedData, blockData, "Block data not stored correctly");
 
         vm.stopPrank();
     }
 
-    // Example test of setting rules, etc.
+    // -----------------------------------------
+    // Tests for inherited OptimisticProposer functions
+    // -----------------------------------------
+
     function testSetRules() public {
         vm.startPrank(owner);
         blockTracker.setRules(newRules);
@@ -94,14 +106,75 @@ contract BlockTrackerTest is Test {
         assertEq(blockTracker.rules(), newRules, "Rules not updated");
     }
 
-    // Example test of setCollateralAndBond
+    function testSetIdentifier() public {
+        // Initially set to 'identifier'
+        assertEq(blockTracker.identifier(), identifier);
+
+        vm.startPrank(owner);
+        blockTracker.setIdentifier(newIdentifier);
+        vm.stopPrank();
+
+        assertEq(blockTracker.identifier(), newIdentifier, "Identifier not updated");
+    }
+
+    function testSetLiveness() public {
+        // Initially set to 'liveness'
+        assertEq(blockTracker.liveness(), liveness);
+
+        vm.startPrank(owner);
+        blockTracker.setLiveness(newLiveness);
+        vm.stopPrank();
+
+        assertEq(blockTracker.liveness(), newLiveness, "Liveness not updated");
+    }
+
+    function testSetEscalationManager() public {
+        vm.startPrank(owner);
+        blockTracker.setEscalationManager(address(mockEscalationManager));
+        vm.stopPrank();
+
+        assertEq(blockTracker.escalationManager(), address(mockEscalationManager), "Escalation manager not updated");
+    }
+
     function testSetCollateralAndBond() public {
-        // "owner" can change
         vm.startPrank(owner);
         blockTracker.setCollateralAndBond(IERC20(address(newMockCollateral)), bondAmount);
         vm.stopPrank();
 
         assertEq(address(blockTracker.collateral()), address(newMockCollateral), "Collateral not updated");
         assertEq(blockTracker.bondAmount(), bondAmount, "Bond amount not updated");
+    }
+
+    function testProposeTransactions() public {
+        // Give txProposer some collateral to pay bond
+        mockCollateral.transfer(txProposer, 3000e18);
+
+        vm.startPrank(txProposer);
+        // Approve enough collateral for the bond
+        mockCollateral.approve(address(blockTracker), 3000e18);
+
+        // Build a small set of transactions
+        OptimisticProposer.Transaction[] memory txs = new OptimisticProposer.Transaction[](2);
+
+        // transaction #1
+        txs[0] = OptimisticProposer.Transaction({
+            to: address(0x1234),
+            operation: Enum.Operation.Call,
+            value: 0,
+            data: ""
+        });
+
+        // transaction #2
+        txs[1] = OptimisticProposer.Transaction({
+            to: address(mockOptimisticOracleV3),
+            operation: Enum.Operation.Call,
+            value: 0,
+            data: "0x"
+        });
+
+        // propose transactions with some "explanation"
+        blockTracker.proposeTransactions(txs, bytes("Hello world"));
+
+        vm.stopPrank();
     }
 }
